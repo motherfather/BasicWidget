0708579de539abc46b8d017268df6156122ce554
motherfather <father@mail.com>
2016-11-23 09:43:38 +0900

    !!

--- app/src/main/java/com/bit2016/basicwidget/MainActivity.java
+++ app/src/main/java/com/bit2016/basicwidget/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 되나요?
+        // 되나요?!!
     }
 }
